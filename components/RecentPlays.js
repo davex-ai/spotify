92186a0ea18b4288c7f10d642926e18773c59c2d
davex-ai <omotoyedave4@gmail.com>
2026-01-15 19:49:19 +0100

    recentplays

--- components/RecentPlays.js
+++ components/RecentPlays.js
@@ -3,11 +3,12 @@ import React from 'react'
 
 const RecentPlays = ({item}) => {
   return (
-    <Pressable>
-        <Image source={{uri: item?.track?.album.images[0].uri}} style={{width: 130, height: 130, borderRadius: 5}}/>
-        <Text>{item?.track?.name}</Text>
+    <Pressable style={{margin: 10}}>
+        <Image source={{uri: item?.track?.album.images[0].url}} style={{width: 130, height: 130, borderRadius: 5}}/>
+        <Text numberOfLines={1} style={{fontSize: 13, fontWeight: 500, color: 'white', marginTop: 10}}>{item?.track?.name}</Text>
     </Pressable>
   )
 }
 
 export default RecentPlays
+// style={{}}
