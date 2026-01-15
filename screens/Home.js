b419cb5f4d6b8eab06aeecd3e5ed1f8d1f9e6bd6
davex-ai <omotoyedave4@gmail.com>
2026-01-15 11:45:57 +0100

    .......

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+// import { LinearGradient } from 'expo-linear-gradient'
+
 
 const Home = () => {
+  const [userProfile, setUserProfile] = useState([])
+  const getProfile = async () => {
+    const accessToken = AsyncStorage.getItem('token')
+    try {
+      const res = await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      const data = await res.json()
+      setUserProfile(data)
+      return data
+    } catch (err) {
+      console.error(err.message);      
+    }
+  }
+
+  
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <LinearGradient colors={['#040306', '#131624']} style={{ flex: 1 }}>
+      <ScrollView style={{ marginTop: 50 }}>
+        <Text>Home</Text>
+      </ScrollView>
+    </LinearGradient>
   )
 }
 
