3ac90ff20e5f0256dc70ab6ada18998391c75490
davex-ai <omotoyedave4@gmail.com>
2026-01-16 22:09:31 +0100

    navigation recentlyplays

--- components/RecentPlays.js
+++ components/RecentPlays.js
@@ -1,9 +1,11 @@
 import { View, Text, Pressable, Image } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const RecentPlays = ({ item }) => {
+  const navigation = useNavigation()
   return (
-    <Pressable style={{ margin: 10 }}>
+    <Pressable onPress={() => navigation.navigate("Info", {item: item})} style={{ margin: 10 }}>
       <Image source={{ uri: item?.track?.album.images[0].url }} style={{ width: 130, height: 130, borderRadius: 5 }} />
       <Text numberOfLines={1} style={{ fontSize: 13, fontWeight: 500, color: 'white', marginTop: 10 }}>{item?.track?.name}</Text>
     </Pressable>
