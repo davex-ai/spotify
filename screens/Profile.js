2b88adebab55bfb7012a9e98d4bca11a607f0635
davex-ai <omotoyedave4@gmail.com>
2026-01-16 23:56:19 +0100

    .....

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,14 +1,55 @@
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import axios from "axios";
+import j from "../assets/icon.png";
+
 
 const Profile = () => {
-  const [userProfile, setUserProfile] = useState()
+  const [userProfile, setUserProfile] = useState(null)
+  const [playlist, setPlaylist] = useState([])
+
   const getProfile = async () => {
-        const accessToken = await AsyncStorage.getItem('token')
+    const accessToken = await AsyncStorage.getItem('token')
+    try {
+      const res = await fetch('https://api.spotify.com/v1/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        })
+        const data = await res.json()
+        setUserProfile(data)
+        return data
+      } catch (err) {
+        console.error(err.message);      
+      }
+    }
+    useEffect(() => {
+      getProfile()
+    },[])
     
-  }
+    useEffect(() => {
+      const getPlaylist = async () => {
+          const accessToken = await AsyncStorage.getItem('token')
+          try {
+                  const res = await axios.get( 
+                     "https://api.spotify.com/v1/me/player/recently-played?limit=6",{
+                         headers: {
+                         Authorization: `Bearer ${accessToken}`
+                       },                      
+                     }
+                  )
+                  setPlaylist(res.data.items)
+                } catch (err) {
+                  console.log("Error",err.message);
+                  
+                } 
+
+        }
+        getPlaylist()
+      },[])
+    
   // const
   return (
     <LinearGradient colors={['#040306', '#131624']} style={{ flex: 1 }}>
@@ -25,8 +66,9 @@ const Profile = () => {
         </View>
         <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold', marginHorizontal: 12}}>Your Playlists</Text>
         <View>
-          {playlists.map((item, index) => {
+          {playlist.map((item, index) => {
             <View>
+              <Image source={{ uri: item?.images[0]?.url || '../assets/icon.png'}} style={{width: 50, height: 50, borderBottomLeftRadius: 4}}/>
               </View>
           })}
         </View>
